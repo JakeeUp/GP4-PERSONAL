03b4a09da256af998c9306670f4a2f6bdd65c100
JakeeUp <fjacob31@yahoo.com>
2024-02-23 18:33:09 -0600

    init

---
+++ Source/BodyCamFPS/BodyCamFPS.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BodyCamFPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BodyCamFPS, "BodyCamFPS" );
+ 
